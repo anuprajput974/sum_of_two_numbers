a7373814186996362a97b4a240e0ee54c636a14a
anuprajput974 <anandnagar3769@gmail.com>
2024-02-18 14:31:37 +0530

    sum of two numbers

---
+++ sum_of_two_numbers/src/sum_of_two_numbers/add_two_numbers.java
@@ -0,0 +1,13 @@
+package sum_of_two_numbers;
+
+public class add_two_numbers {
+
+	public static void main(String[] args) {
+		int x=98;
+		int y=93;
+		int sum=x+y;
+		System.out.println("the sum of two numbers are"+sum);
+
+	}
+
+}
